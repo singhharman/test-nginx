64f270a56c7ad50eeac2b9b2ea5bc24fe7197aaf
harman <harmandps@gmail.com>
2021-02-11 10:34:57 -0500

    added policy document & cron job for nginx

--- iam.tf
+++ iam.tf
@@ -1,44 +1,37 @@
+data "aws_iam_policy_document" "test101_ec2_document" {
+ statement {
+   effect = "Allow"
+   principals { 
+     identifiers = ["ec2.amazonaws.com"]
+     type = "Service"
+   }
+   actions = ["sts:AssumeRole"]
+ }
+}
+
+data "aws_iam_policy_document" "test101_s3_document" {
+ statement {
+   effect = "Allow"
+   actions = ["s3:*"]
+   resources = [ 
+        "arn:aws:s3:::artifacts.harmantraining",
+        "arn:aws:s3:::artifacts.harmantraining/*"
+   ]
+}
+}
+
 resource "aws_iam_role" "test101_role" { 
   name = "test101_role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-  }
-  EOF
+  assume_role_policy = data.aws_iam_policy_document.test101_ec2_document.json
 }
 
 resource "aws_iam_role_policy" "test101_policy" {
   name = "test101_policy"
   role = aws_iam_role.test101_role.id
-policy = <<EOF
-{
-"Version": "2012-10-17",
-"Statement": [
-    {
-        "Sid": "VisualEditor0",
-        "Effect": "Allow",
-        "Action": "s3:*",
-        "Resource": [
-        "arn:aws:s3:::artifacts.harmantraining",
-        "arn:aws:s3:::artifacts.harmantraining/*"
-        ]
-    }
-    ]
-}
-EOF
+  policy = data.aws_iam_policy_document.test101_s3_document.json
 }
 
 resource "aws_iam_instance_profile" "test101_profile" {
     name = "test101_profile"
     role = aws_iam_role.test101_role.name 
-  
 }
--- install-nginx.sh
+++ install-nginx.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 sudo yum update
+# install Nginx
 sudo amazon-linux-extras install -y nginx1
+# Start Nginx Service
 sudo service nginx start
+# Create a txt file and write Nginx version
 test=$(nginx \-v 2>&1)
-sudo chmod 777 /usr/share/nginx/html/
+sudo chmod a+rw /usr/share/nginx/html/
 echo $test > /usr/share/nginx/html/version.txt
-mkdir /home/ec2-user/tools
-sudo chmod 777 /home/ec2-user/tools
-sudo aws s3 cp s3://artifacts.harmantraining/nginx-restart.sh /home/ec2-user/tools
-#*/10 * * * * /home/ec2-user/tools/nginx-restart.sh
+# Make directory for tools
+sudo aws s3 cp s3://artifacts.harmantraining/nginx-restart.sh /usr/bin/
+sudo chmod a+rwx /usr/bin/nginx-restart.sh
+# Schedule nginx restart bash script
+(crontab -l ; echo "*/10 * * * * /usr/bin/nginx-restart.sh")| crontab -
